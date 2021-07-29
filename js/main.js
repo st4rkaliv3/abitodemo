aa18d39ee03040e27e149b340928894d8e7af52e
StarkAlive <st4rkmp@st4rkhplt>
2021-07-29 15:13:26 +0300

    адаптация верстки

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
-const swiper = new Swiper(".mySwiper", {
-	spaceBetween: 10,
-	slidesPerView: 5,
-	freeMode: true,
-	watchSlidesVisibility: true,
-	watchSlidesProgress: true,
-});
-const swiper2 = new Swiper(".mySwiper2", {
-	spaceBetween: 10,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	thumbs: {
-		swiper: swiper,
-	},
-});
+//swiper
+try {
+	const swiper = new Swiper(".mySwiper", {
+		spaceBetween: 10,
+		slidesPerView: 5,
+		freeMode: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+	});
+	const swiper2 = new Swiper(".mySwiper2", {
+		spaceBetween: 10,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		thumbs: {
+			swiper: swiper,
+		},
+	});
+} catch (e) {}
+
+//menu hamburger
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar-panel');
+const menuCloseBtn = document.querySelector('.close-menu');
+if (!!menuBtn && !!menu && !!menuCloseBtn) {
+	const menuToggle = () => {
+		menu.classList.toggle('active');
+	};
+	menuBtn.addEventListener('click', menuToggle);
+	menuCloseBtn.addEventListener('click', menuToggle);
+}
